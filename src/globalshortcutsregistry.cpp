316ba6d488bfcf118e0fcda6fef353d7fe743e05
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2025-01-16 15:40:13 +0200

    Ignore invalid services shortcuts in GlobalShortcutsRegistry::migrateConfig()

--- src/globalshortcutsregistry.cpp
+++ src/globalshortcutsregistry.cpp
@@ -181,8 +181,13 @@ void GlobalShortcutsRegistry::migrateConfig()
                 continue;
             }
 
-            const QString shortcut = value.split(QLatin1Char(','))[0];
-            const QString defaultShortcut = value.split(QLatin1Char(','))[1];
+            const QStringList triplet = value.split(QLatin1Char(','));
+            if (triplet.size() != 3) {
+                continue;
+            }
+
+            const QString shortcut = triplet[0];
+            const QString defaultShortcut = triplet[1];
 
             if (shortcut != defaultShortcut) {
                 newGroup.writeEntry(key, shortcut);
